b5c1c6ebdefd1404ffbe9bee788a8f6b67bab745
Kelvin <kelvinkonoor@gmail.com>
2024-09-21 23:34:44 +0530

    custom DistributedDataLoader based on Karpthy's implementation

---
+++ main/dataloader.py
@@ -0,0 +1,27 @@
+import torch
+from pickle import load
+
+class DistributedDataLoader:
+    def __init__(self, filename: str, B: int, T: int, global_rank: int, world_size: int):
+        self.global_rank = global_rank
+        self.world_size = world_size
+        self.B = B
+        self.T = T
+        # self.bos = [1]
+        # self.eos = [2]
+        self.curr_index = self.global_rank * self.B * self.T
+        with open(filename, 'rb') as f:
+            self.tokens = load(f)
+    
+    def get_batch(self):
+        buffer = self.tokens[self.curr_index: self.curr_index + self.B * self.T + 1]
+        x = torch.tensor(buffer[:-1], dtype=torch.long).view(self.B, self.T)
+        y = torch.tensor(buffer[1:], dtype=torch.long).view(self.B, self.T)
+        self.curr_index += self.world_size * self.B * self.T
+        if (self.curr_index + self.B * self.T + 1) > len(self.tokens):
+            self.curr_index = 0
+        return x, y
+
+
+
+
